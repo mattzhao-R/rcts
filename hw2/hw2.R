ff26a9503a0e0fb63d1e09e954b6234389cd735e
mattzhao-R <mzhao117@uchicago.edu>
2023-10-26 00:51:00 -0500

    added hw2 data file + TA reference code for randomization

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -1,7 +1,36 @@
 # Last updated: Oct 26, 2023
 
-# load packages
+# clear environment
+rm(list = ls())
+
+# load packages + set seed
 pacman::p_load(tidyverse,lubridate,stargazer)
 
-ddir_matt <- './rcts'
+set.seed(2090820)
+
+ddir_matt <- './rcts/hw2/data/'
 ddir <- ddir_matt
+
+# import data
+
+df <- read.csv(paste0(ddir,'brune_et_al_data.csv'))
+
+# Randomization ----
+
+## TA code ----
+### complete rand ----
+n <- df %>% nrow()
+
+df$rand <- runif(n, 0, 1)
+df$rank <- rank(df$rand)
+
+df$treat <- ifelse(df$rank < n / 2, 1, 0)
+
+### strat rand ----
+df$rand <- runif(n, 0, 1)
+
+for (village in 1:max(df$village_id)) {
+  in_village <- (df$village_id == village)
+  str_treat_i <- in_village * (df$rand < median(df$rand[in_village]))
+  df$treat_strat <- df$treat_strat + str_treat_i
+}
