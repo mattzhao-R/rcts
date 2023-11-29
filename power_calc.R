0b836921f315d10f9ad0261e06efc1e3a31571c3
mattzhao-R <mzhao117@uchicago.edu>
2023-11-29 11:54:34 -0600

    pre presentation power calc

--- power_calc.R
+++ power_calc.R
@@ -25,27 +25,32 @@ fake_data %>%
 
 # Power Calculation ----
 ## Main Spec ----
-### Sample Size ----
-#### DGP ----
-apply_dgp <- function(n){
+### DGP ----
+apply_dgp <- function(n,mde){
+  wages <- rep(seq(15,35,5), n / 5)
   rank <- rank(runif(n))
   remoteYes <- ifelse(rank <= length(rank) / 2, 1, 0)
-  baseapplyProb <- rnorm(n, mean = 0.2, sd = 0.3)
-  applyProb <- baseapplyProb + 0.2 * remoteYes
+  baseapplyProb <- rnorm(n, mean = 0.083, sd = 0.25) + 
+    (log10(wages) / 10)
+  baseapplyProbbounded <- ifelse(baseapplyProb <= 0,0,baseapplyProb)
+  applyProb <- baseapplyProbbounded + mde * remoteYes
   applyYes <- ifelse(applyProb >= 0.5, 1, 0)
+  
   
   apply_data <- data.frame(
     applyYes,
     remoteYes,
     baseapplyProb,
     applyProb,
-    applyYes
+    applyYes,
+    wages
   )
   
   apply_data
 }
 
-#### Simulation + Graph ----
+### Sample Size ----
+
 power_vec <- c()
 n_vals <- seq(50,200,10)
 
@@ -53,7 +58,7 @@ for (n in n_vals) {
   pvals <- c()
   
   for (i in 1:1000) {
-    df <- apply_dgp(n)
+    df <- apply_dgp(n,mde=0.2)
     model <- lm(applyYes ~ remoteYes, data = df)
     pval <- summary(model)$coefficients['remoteYes','Pr(>|t|)']
     pvals <- c(pvals,pval)
@@ -65,7 +70,70 @@ for (n in n_vals) {
 
 ggplot(mapping = aes(x = n_vals, y = power_vec)) + 
   geom_point() + 
-  labs(x = 'Sample Size', y = 'Power (%)')
+  labs(x = 'Sample Size', y = 'Power (%)') +
+  ggtitle('Necessary Sample Size at MDE = 0.2') +
+  theme(plot.title = element_text(family = 'serif', hjust = 0.5, size = 12), 
+        axis.title = element_text(family = 'serif', size = 10),
+        axis.text = element_text(family = 'serif',size=8)) + 
+  geom_hline(yintercept = 80, alpha = 0.5, linetype = 2)
+ggsave(filename = 'main_n_base.png',
+       path = "./rcts/power_outputs",
+       device = "png",
+       width = 15,
+       height = 8,
+       limitsize = FALSE,
+       dpi = 300,
+       units = "cm")
+
+### MDE Sensitivity ----
+
+mde_vals <- seq(0.05,0.35,0.05)
+n_vals <- seq(50,200,10)
+sens_df <- data.frame()
+
+for (n in n_vals) {
+  power_vec <- c()
+  for (mde in mde_vals) {
+    pvals <- c()
+    
+    for (i in 1:1000) {
+      df <- apply_dgp(n,mde)
+      model <- lm(applyYes ~ remoteYes, data = df)
+      pval <- summary(model)$coefficients['remoteYes','Pr(>|t|)']
+      pvals <- c(pvals,pval)
+    }
+    
+    power <- sum(pvals < 0.05) / 1000 * 100
+    power_vec <- c(power_vec, power)
+  }
+  temp <- data.frame(
+    power_vec,
+    mde_vals,
+    rep(n,length(power_vec))
+  )
+  sens_df <- bind_rows(sens_df, temp)
+}
+colnames(sens_df) <- c('power','mde','n')
+
+ggplot(data = sens_df %>%
+         filter(!(mde %in% c(0.05,0.35))),
+       mapping = aes(x = n, y = power, colour = factor(mde))) + 
+  # geom_point() + 
+  geom_smooth(method = 'loess', se = F) + 
+  labs(x = 'Sample Size', y = 'Power (%)', colour = 'MDE') +
+  ggtitle('Power Sensitivity to MDE') +
+  theme(plot.title = element_text(family = 'serif', hjust = 0.5, size = 12), 
+        axis.title = element_text(family = 'serif', size = 10),
+        axis.text = element_text(family = 'serif',size=8)) + 
+  geom_hline(yintercept = 80, alpha = 0.5, linetype = 2)
+ggsave(filename = 'main_sens_base.png',
+       path = "./rcts/power_outputs",
+       device = "png",
+       width = 15,
+       height = 8,
+       limitsize = FALSE,
+       dpi = 300,
+       units = "cm")
 
 
 ## Secondary Spec ----
